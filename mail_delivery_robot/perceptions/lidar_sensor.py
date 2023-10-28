4b2f769631640c9051a0d730d79ed727313ca391
Bardia Parmoun <bardiaparmoun@gmail.com>
2023-10-27 22:53:15 -0400

    a proposed change for the lidar sensor.

--- mail_delivery_robot/perceptions/lidar_sensor.py
+++ mail_delivery_robot/perceptions/lidar_sensor.py
@@ -7,28 +7,10 @@ import sys
 from sensor_msgs.msg import LaserScan
 from statistics import  mean, stdev
 
-TIMER_PERIOD = 0.07 #seconds
+TIMER_PERIOD = 0.1 #seconds
 FORWARD_X_SPEED = 0.2 #m/s
 
 class LidarSensor(Node):
-    """
-    A class to represent the IR Sensors.
-
-    ...
-
-    Attributes
-    ----------
-    publisher : Publisher
-        The classes publisher.
-    timer : Timer
-        The timer will be started and every timer_period_sec number of 
-        seconds the provided callback function will be called.
-
-    Methods
-    -------
-    sendReading():
-        Publishes the reading from the IR sensors to the perceptions node.
-    """ 
     def __init__(self):
         super().__init__('lidar_sensor')
         self.publisher_ = self.create_publisher(String, 'perceptions' , 10)
@@ -36,9 +18,9 @@ class LidarSensor(Node):
 
         self.pid_controller = PID(1.1,0,0) #init pid controller
         
-        self.pid_controller.SetPoint = 0.4
         self.pid_controller.setSampleTime(TIMER_PERIOD)
         self.scan = []
+        self.stack = [0,0,0,0,0]
 
     def scan_callback(self, scan):
         count = int(scan.scan_time / scan.time_increment)
@@ -48,7 +30,7 @@ class LidarSensor(Node):
         calc = String()
         
         #update pid controller and get output
-        feedback, angle = calculate(self.scan)
+        feedback, angle = self.calculate(self.scan)
         #self.get_logger().info("[SLLIDAR INFO]: distance {} and angle {}]".format(feedback, angle))
         if feedback == -1 and angle == -1: 
             calc.data = "-1"
@@ -59,149 +41,47 @@ class LidarSensor(Node):
 
         self.publisher_.publish(calc)
 
-
-
-    def sendReading(self):
-        calc = String()
-        
-        #update pid controller and get output
-        feedback, angle = calculate(self.scan)
-        if feedback == -1 and angle == -1: 
-            calc.data = "-1"
-        else: 
-            self.pid_controller.update(feedback)
-            output = self.pid_controller.output
-            calc.data = str(output)
-
-        self.publisher_.publish(calc)
-
-
-# Create an ADS1115 ADC (16-bit) instance.
-#adc = Adafruit_ADS1x15.ADS1115()
-stack1 = [0,0,0,0,0]  
-stack2 = [0,0,0,0,0] 
-avg1 = 0
-avg2 = 0
-# See table 3 in the ADS1015/ADS1115 datasheet for more info on gain.
-GAIN = 2
-
-
-def calculate(scan):
-    '''
-    Takes values recieved from the IR sensors and adds them to a stack. Then 
-    when a new reading is collected it's compared to the average of the last 5,
-    if it deviates too much from the mean it ignores the value but still adds it to the stack.
-
-            Returns:
-                    distance_angle_calc (str): A string tuple containing the distance_from_wall (cm)
-                    and the angle_between_wall (deg) in the format: 'A,B'. Else returns -1
-                    if it is an outlier measurment.
-    '''        
-    values = [0] * 2
-    count = int(scan.scan_time / scan.time_increment)
-    angle = 0
-    min_distance = 10
-    distances = []
-    max_distance = 0
-    for i in range(count):
-        degree = math.degrees(scan.angle_min + scan.angle_increment * i)
-        if degree >= 30 and degree <= 150:
+    def calculate(self, scan):     
+        count = int(scan.scan_time / scan.time_increment)
+        angle = 0
+        min_distance = 10
+        max_pos_distance = 0
+        max_neg_distance = 0
+        for i in range(count):
+            degree = math.degrees(scan.angle_min + scan.angle_increment * i)
             if scan.ranges[i] == math.inf:
                 curDir = 0.5
-            elif scan.ranges[i] == -math.inf:
-                curDir = -0.5
             else:
                 curDir = scan.ranges[i]
 
-            distances.append(curDir)
-            if curDir < min_distance:
+            if degree >= 30 and degree <= 150 and curDir < min_distance:
                 angle = degree
                 min_distance = curDir
 
             if degree >= 60 and degree <= 90:
-                max_distance = max(curDir, max_distance)
+                max_pos_distance = max(curDir, max_pos_distance)
 
-    if max_distance > 2:
-        return -1,-1
+            if degree >= -90 and degree <= -60:
+                max_neg_distance = min(curDir, max_neg_distance)
 
-    #calculate averages
-    avg1 = sum(stack1)/5
-    avg2 = sum(stack2)/5
-    
-    #insert new values, pop oldest values.
-    stack1.insert(0, min_distance)
-    stack1.pop(5)
-    
-    #check if the values are in the range and valid
-    #TODO previous groups have mentioned that this could use tuning
-    if min_distance < avg1*1.5 and min_distance > avg1*0.5:
-        return min_distance, abs(90 - angle)
-    
-    return 0.5, 0
-'''
-    #insert new values, pop oldest values.
-    if(len(stack1) < 10 and len(stack2) < 10):
-        stack1.insert(0, values[0])
-        stack2.insert(0, values[0]) 
-    else: 
-        avg1 = sum(stack1)/len(stack1)
-        avg2 = sum(stack2)/len(stack2)
+        if max_pos_distance > 2 and max_neg_distance > 4:
+            self.stack = [0,0,0,0,0]
+            self.pid_controller.clear()
+            return -1,-1
+
+        #calculate averages
+        avg1 = sum(self.stack)/5
         
-        if values[0] < 0.15*1.5:
-            if values[1] < 0.15*1.5:
-                stack1.insert(0, values[0])
-                stack2.insert(0, values[0]) 
-
-                stack1.pop(len(stack1) - 1)
-                stack2.pop(len(stack2) - 1)
-
-                return distances(values[0], values[1])
-    
-    
-    #prints values and averages for testing
-    #print("Sensor1: " + str(values[0]) + "    avg: " + str(avg1))
-    #print("Sensor2: " + str(values[1]) + "    avg: " + str(avg2))
-    
-    #check if the values are in the range and valid
-    #TODO previous groups have mentioned that this could use tuning
-    
-    return -1,-1
-   ''' 
-
-def distance(sensor1_distance, sensor2_distance):
-    '''
-    Returns the distance the robot is from the wall and its current angle.
-    The diagram explaining the relation between variable names and the IR sensors
-    can be found in the documentation.
-    
-            Parameters:
-                    sensor1_distance (float): Distance reading (cm) from IR sensor 1
-                    sensor2_distance (float): Distance reading (cm) from IR sensor 2
-
-            Returns:
-                    distance_angle_calc (str): A string tuple containing the distance_from_wall (cm)
-                    and the angle_between_wall (deg) in the format: 'A,B'
-    '''
+        #insert new values, pop oldest values.
+        self.stack.insert(0, min_distance)
+        self.stack.pop(5)
         
-
-    # set angle_between_sensors as the angle between the two IR sensors
-    angle_between_sensors = 30
-
-    # a is the distance between where the two censors make contact with the wall
-    c = math.sqrt((sensor1_distance ** 2 + sensor2_distance ** 2) - (2 * sensor1_distance * sensor2_distance *
-                                                                     math.cos(angle_between_sensors * math.pi / 180)))
-
-    # this is to find angle A, which tells us if the robot is moving towards or away from the wall
-    # an obtuse angle (greater than 90 degrees) means it is moving away
-    # an acute angle (less than 90 degrees) means it is moving towards
-    angle_between_wall = math.acos(
-        (sensor1_distance ** 2 + c ** 2 - sensor2_distance ** 2) / (2 * sensor1_distance * c)) * 180 / math.pi
-
-    distance_from_wall = sensor1_distance * math.sin(angle_between_wall * math.pi / 180)
-
-    #this is returning the distance from the wall in metres and the angular between the wall in degrees
-    distance_angle_calc = (abs(distance_from_wall),angle_between_wall)
-    return distance_angle_calc
+        #check if the values are in the range and valid
+        #TODO previous groups have mentioned that this could use tuning
+        if min_distance < avg1*1.5 and min_distance > avg1*0.5:
+            return min_distance, abs(90 - angle)
+        
+        return 0.5, 0
     
 class PID:
     """PID Controller
@@ -213,7 +93,7 @@ class PID:
         self.Ki = I
         self.Kd = D
 
-        self.sample_time = 0.1
+        self.sample_time = 0.0
         self.current_time = current_time if current_time is not None else time.time()
         self.last_time = self.current_time
 
@@ -221,7 +101,7 @@ class PID:
 
     def clear(self):
         """Clears PID computations and coefficients"""
-        self.SetPoint = 0.0
+        self.SetPoint = 0.4
 
         self.PTerm = 0.0
         self.ITerm = 0.0
@@ -296,7 +176,6 @@ def main():
     rclpy.init()
     lidar_sensor = LidarSensor()
 
-    # Run irsensor when not blocked by shutdown
     rclpy.spin(lidar_sensor)
     
 if __name__ == '__main__':
