22156032194e6f6b3015e8855f99adf5ef199292
Cassidy Pacada <cassidy.pacada@gmail.com>
2023-12-03 06:36:03 -0800

    refactored and added testing for expected actions

--- mail_delivery_robot/test/test_state_machine.py
+++ mail_delivery_robot/test/test_state_machine.py
@@ -1,18 +1,36 @@
 from control import state_machine as state_machine
 
 class ActionPublisherStub():
+    '''
+    The stub for the Action Publisher in the state machine
+    There is nothing actually being published, it just stores a list of completed actions
+    '''
+
     def __init__(self):
         self.actions = []
+        self.action_data = []
 
     def publish(self, action):
         self.actions.append(action)
 
+    
+    def extract_data(self):
+        '''
+        Puts all the friendly names for completed actions into a list for easy comparison
+        '''
+        for action in self.actions:
+            self.action_data.append(action.data.split(":")[0])
 
-def test_all_states():
+
+def test_path_one():
+    '''
+    Test Path 1: Asserts that state transitions are good for right turns and that the completed actions
+    are as expected
+    '''
+
     actionPublisher = ActionPublisherStub()
     state = state_machine.No_Dest(actionPublisher)
 
-    # Path 1
     state = state.handleUpdate(False, "NAV_NONE", "10:0")
     assert state.stateType.value == "NO_DEST"
     state = state.handleUpdate(True, "NAV_NONE", "10:-0")
@@ -24,7 +42,19 @@ def test_all_states():
     state = state.handleUpdate(False, "NAV_NONE", "-1:-1")
     assert state.stateType.value == "HANDLE_INTERSECTION"
 
-    #Path 2
+    actionPublisher.extract_data()
+    expected_actions = ["WALL_FOLLOW", "L_TURN", "L_TURN", "R_TURN", "R_TURN"]
+    assert actionPublisher.action_data == expected_actions
+
+def test_path_two():
+    '''
+    Test Path 2: Asserts that state transitions are good for left turns and that the completed actions
+    are as expected
+    '''
+
+    actionPublisher = ActionPublisherStub()
+    state = state_machine.No_Dest(actionPublisher)
+
     state = state.handleUpdate(False, "NAV_NONE", "10:0")
     assert state.stateType.value == "NO_DEST"
     state = state.handleUpdate(True, "NAV_LEFT", "10:0")
@@ -38,17 +68,42 @@ def test_all_states():
     state = state.handleUpdate(False, "NAV_LEFT", "-1:-1")
     assert state.stateType.value == "HANDLE_INTERSECTION"
 
-    #Path 3
+    actionPublisher.extract_data()
+    expected_actions = ["WALL_FOLLOW", "L_TURN", "R_TURN", "L_TURN", "L_TURN", "R_TURN"]
+    assert actionPublisher.action_data == expected_actions
+
+
+def test_path_three():
+    '''
+    Test Path 3: Asserts that state transitions are good for passing and that the completed actions
+    are as expected
+    '''
+
+    actionPublisher = ActionPublisherStub()
+    state = state_machine.No_Dest(actionPublisher)
+
     state = state.handleUpdate(False, "NAV_NONE", "10:0")
     assert state.stateType.value == "NO_DEST"
     state = state.handleUpdate(True, "NAV_PASS", "10:0")
     assert state.stateType.value == "COLLISION_PASS"
-    state = state.handleUpdate(False, "NAV_PASS", "10:0") #False with NAV_PASS but works with other values... not sure why
+    state = state.handleUpdate(False, "NAV_PASS", "10:0") 
     assert state.stateType.value == "SHOULD_PASS", "THIS IS AN ERROR"
     state = state.handleUpdate(False, "NAV_PASS", "-1:-1")
     assert state.stateType.value == "HANDLE_INTERSECTION"
 
-    #Path 4
+    actionPublisher.extract_data()
+    expected_actions = ["WALL_FOLLOW", "L_TURN", "R_TURN", "FORWARD"]
+    assert actionPublisher.action_data == expected_actions
+
+def test_path_four():
+    '''
+    Test Path 4: Asserts that state transitions are good for docking and that the completed actions
+    are as expected
+    '''
+
+    actionPublisher = ActionPublisherStub()
+    state = state_machine.No_Dest(actionPublisher)
+
     state = state.handleUpdate(False, "NAV_NONE", "10:0")
     assert state.stateType.value == "NO_DEST"
     state = state.handleUpdate(True, "NAV_DOCK", "10:0")
@@ -57,6 +112,18 @@ def test_all_states():
     assert state.stateType.value == "SHOULD_DOCK"
     state = state.handleUpdate(False, "NAV_DOCK", "-1:-1")
     assert state.stateType.value == "DOCKED"
+
+    actionPublisher.extract_data()
+    expected_actions = ["WALL_FOLLOW", "L_TURN", "R_TURN", "DOCK"]
+    assert actionPublisher.action_data == expected_actions
+
+
+def test_path_five():
+    '''
+    Test Path 5: Asserts that state the state transition into NOT_OPERATIONAL is good
+    '''
+    actionPublisher = ActionPublisherStub()
+    state = state_machine.No_Dest(actionPublisher)
 
     #Path 5
     state = state_machine.No_Dest(actionPublisher)
