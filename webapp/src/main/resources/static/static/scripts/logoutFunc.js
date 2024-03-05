803c453295c969d26420921889ae77cde2709a3c
Max Curkovic <deekey45@gmail.com>
2024-03-04 22:03:29 -0500

    Adding logout functionality

--- webapp/src/main/resources/static/static/scripts/logoutFunc.js
+++ webapp/src/main/resources/static/static/scripts/logoutFunc.js
@@ -1,17 +1,5 @@
 const logoutButton = $("#logout-btn");
-const profileName = $("#profile-name");
-const dropDownSelections = $("#drop-down-selections");
 
-/**
- * drop down the nav menu for when you are logged in
- */
-profileName.click(() => {
-    if (!dropDownSelections.hasClass("visible")) {
-        dropDownSelections.addClass("visible");
-        return;
-    }
-    dropDownSelections.removeClass("visible");
-});
 
 /**
  * The JavaScript AJAX call for when a user logs out of their account
