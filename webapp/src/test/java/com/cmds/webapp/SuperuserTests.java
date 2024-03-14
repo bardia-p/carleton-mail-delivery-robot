ac5ad03658fc2aa9987690a31f262a0ccf816592
maxcurkovic <deekey45@gmail.com>
2024-03-14 11:24:59 -0400

    Added PageController tests

--- webapp/src/test/java/com/cmds/webapp/SuperuserTests.java
+++ webapp/src/test/java/com/cmds/webapp/SuperuserTests.java
@@ -4,10 +4,13 @@ import com.cmds.webapp.models.AppUser;
 import com.cmds.webapp.models.Delivery;
 import com.cmds.webapp.models.Superuser;
 import com.cmds.webapp.repos.DeliveryRepository;
+import com.cmds.webapp.repos.SuperuserRepository;
 import com.cmds.webapp.repos.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -24,6 +27,9 @@ public class SuperuserTests {
     @Autowired
     private DeliveryRepository deliveryRepo;
 
+    @Autowired
+    private SuperuserRepository superuserRepo;
+
     /**
      * Test for setting the current delivery of a superuser.
      */
@@ -31,12 +37,11 @@ public class SuperuserTests {
     public void testSetCurrentDelivery() {
         Superuser user = new Superuser("test2", "test");
         Delivery delivery = new Delivery();
-        userRepo.save(user);
+        superuserRepo.save(user);
         deliveryRepo.save(delivery);
         assertNull(user.getCurrentDelivery());
         user.setCurrentDelivery(delivery);
         assertEquals(user.getCurrentDelivery(), delivery);
     }
-
 
 }
